0c866a9337b5539477e8317e7d88c7fbfd9ebd1d
yujin9619 <yujin9588@gmail.com>
2021-08-19 12:21:34 +0900

    버튼

--- app/src/main/java/com/nepplus/a0819/MainActivity.kt
+++ app/src/main/java/com/nepplus/a0819/MainActivity.kt
@@ -9,16 +9,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToTextViewBtn.setOnClickListener {
+
+//            입력한 메세지를 -> 변수에 저장해서 -> 텍스트뷰의 -> text 속성으로 대입
+
+            val inputMessage = messageEdt.text.toString()
+
+            resultText.text = inputMessage
+
+        }
+
+
  toastBtn.setOnClickListener {
-
-
-     val inputMessage = messageEdt.text.toString()
 
      //     입력한 메세지가 무너지? 변수에 저장.
 //         messageEdtdml text속성에 담겨있는 값을 => inputmessage 변수에 담자.
 
+     val inputMessage = messageEdt.text.toString()
 
-//     토스트로 inputmessage를 띄ㅜ어주자.
+
+//     토스트로 inputmessage를 띄워주자.
+
      Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
  }
 
